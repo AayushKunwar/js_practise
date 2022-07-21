830ca801e3a40669085272120c500f8f3b41b7f4
Aayush Kunwar <kunwar_aayush@live.com>
2022-07-21 19:59:08 +0545

    added remove button

--- projects/list/index.js
+++ projects/list/index.js
@@ -1,14 +1,32 @@
 const list = document.querySelector("ul");
-
-console.log(list);
+const closeBtns = document.querySelectorAll(".close");
 
 document.querySelector(".add-list").addEventListener("click", function () {
+	console.log("enter button clicked");
 	const text = document.querySelector("#text");
-	const li = document.createElement("li");
 	if (text.value === "") {
 		return;
 	}
-	li.appendChild(document.createTextNode(text.value));
+
+	addListItem(text);
+});
+
+function addListItem(text) {
+	const li = document.createElement("li");
+	const liDiv = document.createElement("div");
+	const closeBtn = document.createElement("span");
+
+	closeBtn.classList.add("close");
+	closeBtn.appendChild(document.createTextNode("\u00D7"));
+	liDiv.classList.add("list-item");
+	liDiv.appendChild(document.createTextNode(text.value));
+	liDiv.appendChild(closeBtn);
+	li.appendChild(liDiv);
 	list.appendChild(li);
 	text.value = "";
-});
+
+	closeBtn.addEventListener("click", function () {
+		li.remove();
+	});
+	console.log("added");
+}
